90628051023bbca27b12976b961521a4dad548ec
Josué Molano Uribe <josuemolanouribe@mac.home>
2026-06-15 12:51:10 -0400

    Re-add Meta Pixel with new ID 952496337835266
    Same setup as before:
    - Base pixel + PageView in root layout (afterInteractive)
    - Lead event fired on submit only when score >= 50

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
+
+const FB_PIXEL_ID = "952496337835266";
 
 const inter = Inter({
   variable: "--font-sans",
@@ -32,6 +35,20 @@ export default function RootLayout({
       className={`${inter.variable} ${playfair.variable} h-full antialiased`}
     >
       <body className="min-h-full bg-white text-[#0a2540] font-sans">
+        {/* Meta (Facebook) Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${FB_PIXEL_ID}');fbq('track','PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
